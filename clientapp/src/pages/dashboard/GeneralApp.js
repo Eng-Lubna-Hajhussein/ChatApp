17ebf6e5ab14b071d1167f91a98c8799a55af250
Lubna Hajhussein <hajhussein.lubna@gmail.com>
2024-03-07 00:29:47 +0300

    header section, footer section & conversation section design

--- clientapp/src/pages/dashboard/GeneralApp.js
+++ clientapp/src/pages/dashboard/GeneralApp.js
@@ -1,15 +1,27 @@
 import React from "react";
 import Chats from "./Chats";
-import { Stack } from "@mui/material";
+import { Box, Stack } from "@mui/material";
+import { useTheme } from "@mui/material/styles";
+import Conversation from "../../components/Conversation";
 
 const GeneralApp = () => {
-
+  const theme = useTheme();
   return (
     <>
-    <Stack direction={'row'} width={'100%'}>
-      {/* Chats */}
-      <Chats />
-    </Stack>
+      <Stack direction={"row"} sx={{ width: "100%" }}>
+        {/* Chats */}
+        <Chats />
+        <Box
+          sx={{
+            height: "100%",
+            width: "calc(100vw - 420px)",
+            backgroundColor:theme.palette.mode==="light"? "#fff":theme.palette.background.default,
+          }}
+        >
+          {/* Conversation */}
+          <Conversation />
+        </Box>
+      </Stack>
     </>
   );
 };
