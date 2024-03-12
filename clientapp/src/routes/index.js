c9d423d6b9afd063f4919e4dfd431abccc385f2a
Lubna Hajhussein <hajhussein.lubna@gmail.com>
2024-03-12 22:58:22 +0300

    login page setup

--- clientapp/src/routes/index.js
+++ clientapp/src/routes/index.js
@@ -3,6 +3,7 @@ import { Navigate, useRoutes } from "react-router-dom";
 
 // layouts
 import DashboardLayout from "../layouts/dashboard";
+import MainLayout from "../layouts/main";
 
 // config
 import { DEFAULT_PATH } from "../config";
@@ -19,6 +20,11 @@ const Loadable = (Component) => (props) => {
 export default function Router() {
   return useRoutes([
     {
+      path: "/auth",
+      element: <MainLayout />,
+      children: [{ element: <LoginPage />, path: "login" }],
+    },
+    {
       path: "/",
       element: <DashboardLayout />,
       children: [
@@ -34,9 +40,8 @@ export default function Router() {
 }
 
 const GeneralApp = Loadable(
-  lazy(() => import("../pages/dashboard/GeneralApp")),
+  lazy(() => import("../pages/dashboard/GeneralApp"))
 );
-const Settings = Loadable(
-  lazy(() => import("../pages/dashboard/Settings")),
-);
+const LoginPage = Loadable(lazy(() => import("../pages/auth/Login")));
+const Settings = Loadable(lazy(() => import("../pages/dashboard/Settings")));
 const Page404 = Loadable(lazy(() => import("../pages/Page404")));
